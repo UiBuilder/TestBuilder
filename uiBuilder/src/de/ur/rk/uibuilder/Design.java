84473cd80729214f46c2c1ede30dc821d1ac367a
jonesses <mail@jonesses.com>
2013-02-16 16:09:06 +0100

    false

--- uiBuilder/src/de/ur/rk/uibuilder/Design.java
+++ uiBuilder/src/de/ur/rk/uibuilder/Design.java
@@ -16,7 +16,7 @@ public class Design extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.design, menu);
-		return true;
+		return false;
 	}
 
 }
