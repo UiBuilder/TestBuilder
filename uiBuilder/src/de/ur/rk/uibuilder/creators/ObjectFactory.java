54f51ecada12132d66885136b09812afdb2e3743
Funklos <fulosio@gmail.com>
2013-02-16 19:25:26 +0100

    ADDED 	some JavaDoc 	zum Verständnis

--- uiBuilder/src/de/ur/rk/uibuilder/creators/ObjectFactory.java
+++ uiBuilder/src/de/ur/rk/uibuilder/creators/ObjectFactory.java
@@ -9,19 +9,44 @@ import android.widget.Button;
 
 public class ObjectFactory {
 
-	public static final int ID_BUTTON = 1;
-	public static final int ID_TEXTVIEW = 2;
+	public static final int ID_BUTTON = 1; /** Konstante für Buttons */
+	public static final int ID_TEXTVIEW = 2; /** Konstante für TextViews */
 	
 	private Context ref;
 	private int idCount;
 	
+	private int displayWidth;
+	private int displayHeight;
+	
+	
+	/**
+	 * KONSTRUKTOR
+	 * 
+	 * @param c Referenz auf die Activity
+	 */
 	public ObjectFactory(Context c) {
 		super();
 		// TODO Auto-generated constructor stub
 		ref = c;
 		idCount = 1;
+		
+		measure();
 	}
 
+	/**
+	 * Aktuelle Displaygröße ermitteln
+	 */
+	private void measure() {
+		// TODO Auto-generated method stub
+		displayHeight = ref.getResources().getDisplayMetrics().heightPixels;
+		displayWidth = ref.getResources().getDisplayMetrics().widthPixels;
+	}
+
+	/**
+	 * 
+	 * @param which Definiert die Art des zu erzeugenden Elementes
+	 * @return Das erzeugte Element
+	 */
 	public View getElement (int which)
 	{
 		switch (which)
@@ -35,11 +60,21 @@ public class ObjectFactory {
 		return null;
 	}
 
+	/**
+	 * Methode zur Generierung eines neuen TextView-Objekts.
+	 * Default Eigenschaften werden gesetzt.
+	 * @return Neuer TextView
+	 */
 	private View newTextview() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Methode zur Generierung eines neuen Button-Objekts.
+	 * Default Eigenschaften werden gesetzt.
+	 * @return Neuer Button 
+	 */
 	private View newButton() {
 		// TODO Auto-generated method stub
 		Button newB = new Button(ref);
