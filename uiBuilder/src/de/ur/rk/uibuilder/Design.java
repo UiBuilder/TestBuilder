03ecb6d53c8713d4b9a863314a3e28ac1c737a89
Funklos <fulosio@gmail.com>
2013-02-16 15:52:02 +0100

    nei damit

---
+++ uiBuilder/src/de/ur/rk/uibuilder/Design.java
@@ -0,0 +1,22 @@
+package de.ur.rk.uibuilder;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Design extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_layout_design);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.design, menu);
+		return true;
+	}
+
+}
