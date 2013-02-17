58b2169a64ad0eb59365112f7464d3d5c0150bc9
Funklos <fulosio@gmail.com>
2013-02-17 09:06:26 +0100

    HA wir müssen kein absolute layout verwenden! relative unterstützt auch das manuelle setzen von absoluten pixelkoordinaten. das ist super weil absolute "deprecated" ist und wir nicht überall "@suppress" warnings im code haben.

--- uiBuilder/src/de/ur/rk/uibuilder/Design.java
+++ uiBuilder/src/de/ur/rk/uibuilder/Design.java
@@ -1,23 +1,25 @@
 package de.ur.rk.uibuilder;
 
-import de.ur.rk.uibuilder.creators.ObjectFactory;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AbsoluteLayout;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import de.ur.rk.uibuilder.creators.ObjectFactory;
 
-@SuppressWarnings("deprecation")
-public class Design extends Activity {
 
-	private AbsoluteLayout root;
+public class Design extends Activity 
+{
+
+	private RelativeLayout root;
 	private Button addView;
 	
 	private ObjectFactory factory; 
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_layout_design);
 		
@@ -25,31 +27,32 @@ public class Design extends Activity {
 		initHelpers();
 	}
 
-	private void initHelpers() {
-		// TODO Auto-generated method stub
+	private void initHelpers() 
+	{
 		factory = new ObjectFactory(getApplicationContext());
 	}
 
-	private void linkElements() {
-		// TODO Auto-generated method stub
-		root = (AbsoluteLayout) findViewById(R.id.design_area);
+	private void linkElements() 
+	{
+		root = (RelativeLayout) findViewById(R.id.design_area);
 		addView = (Button) findViewById(R.id.design_button_add);
 		
 		setListeners();
 	}
 
-	private void setListeners() {
-		// TODO Auto-generated method stub
-		addView.setOnClickListener(new View.OnClickListener() {
+	private void setListeners() 
+	{
+		addView.setOnClickListener(new View.OnClickListener() 
+		{
 			
 			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
+			public void onClick(View v) 
+			{
 				newView();
 			}
 
-			private void newView() {
-				// TODO Auto-generated method stub
+			private void newView() 
+			{
 				Button newOne = (Button) factory.getElement(ObjectFactory.ID_BUTTON);
 				root.addView(newOne);
 			}
@@ -57,10 +60,10 @@ public class Design extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.design, menu);
 		return false;
 	}
-//test
 }
