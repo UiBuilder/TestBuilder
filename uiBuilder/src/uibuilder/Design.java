67abacd945d55374e1e1469954fbdb8715a963bb
jonesses <mail@jonesses.com>
2013-02-18 14:15:21 +0100

    Implementiert rudimentäre touch to create Funktionalität

--- uiBuilder/src/uibuilder/Design.java
+++ uiBuilder/src/uibuilder/Design.java
@@ -1,112 +1,117 @@
 package uibuilder;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.DragEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnDragListener;
-import android.view.ViewGroup.MarginLayoutParams;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import creators.ObjectFactory;
 import de.ur.rk.uibuilder.R;
 
-
-public class Design extends Activity 
-{
+public class Design extends Activity {
 	private float dragx;
 	private float dragy;
-	
+
 	private RelativeLayout root;
-	private Button addView;
-	
-	private ObjectFactory factory; 
-	
+	// private Button addView;
+
+	private ObjectFactory factory;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_layout_design);
-		
+
 		linkElements();
 		initHelpers();
 	}
 
-	private void initHelpers() 
-	{
+	private void initHelpers() {
 		factory = new ObjectFactory(getApplicationContext());
 	}
 
-	private void linkElements() 
-	{
+	private void linkElements() {
 		root = (RelativeLayout) findViewById(R.id.design_area);
-		addView = (Button) findViewById(R.id.design_button_add);
-		
+		// addView = (Button) findViewById(R.id.design_button_add);
+
 		setListeners();
 	}
 
-	private void setListeners() 
-	{
-		addView.setOnClickListener(new View.OnClickListener() 
-		{
-			
-			@Override
-			public void onClick(View v) 
-			{
-				newView();
-			}
+	private void setListeners() {
+		root.setOnTouchListener(new OnTouchListener() {
 
-			private void newView() 
-			{
-				Button newOne = (Button) factory.getElement(ObjectFactory.ID_BUTTON);
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				// holt die Koordinaten des Touch-Punktes
+				float clickPosX = event.getAxisValue(MotionEvent.AXIS_X);
+				float clickPosY = event.getAxisValue(MotionEvent.AXIS_Y);
+				// erstellt den Button an den zuvor ermittelten Koordinaten
+				Button newOne = (Button) factory
+						.getElement(ObjectFactory.ID_BUTTON);
+				newOne.setX(clickPosX);
+				newOne.setY(clickPosY);
 				root.addView(newOne);
-				root.invalidate();
+				return true;
 			}
 		});
-		
-		root.setOnDragListener(new OnDragListener()
-		{
+
+		root.setOnDragListener(new OnDragListener() {
 
 			@Override
-			public boolean onDrag(View v, DragEvent event)
-			{
-				
-				switch(event.getAction())
-				{
-			    case DragEvent.ACTION_DRAG_STARTED:
-			    	
-			        break;
-			    case DragEvent.ACTION_DRAG_ENTERED:
-			        break;
-			    case DragEvent.ACTION_DRAG_LOCATION:
-			        break;
-			    case DragEvent.ACTION_DRAG_ENDED:
-			        break;
-			    case DragEvent.ACTION_DRAG_EXITED:
-			        break;
-			    case DragEvent.ACTION_DROP:
-			    	v.setX(event.getX());
-			    	v.setY(event.getY());
-			    	/*MarginLayoutParams marginParams = new MarginLayoutParams(v.getLayoutParams());
-			    	
-			    	marginParams.topMargin = (int)event.getY();// - (v.getHeight()); 
-			    	marginParams.leftMargin = (int)event.getX();// - (v.getWidth()/2);
-			    	
-			    	RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(marginParams);
-			    	Log.d("paramstop", String.valueOf(marginParams.topMargin));
-			    	v.setLayoutParams(params);
-			    	root.invalidate();*/
+			public boolean onDrag(View v, DragEvent event) {
+
+				switch (event.getAction()) {
+				case DragEvent.ACTION_DRAG_STARTED:
+
+					break;
+				case DragEvent.ACTION_DRAG_ENTERED:
+					break;
+				case DragEvent.ACTION_DRAG_LOCATION:
+
+					break;
+				case DragEvent.ACTION_DRAG_ENDED:
+
+					break;
+				case DragEvent.ACTION_DRAG_EXITED:
+					break;
+				case DragEvent.ACTION_DROP:
+					v.setX(event.getX());
+					v.setY(event.getY());
+					/*
+					 * MarginLayoutParams marginParams = new
+					 * MarginLayoutParams(v.getLayoutParams());
+					 * 
+					 * marginParams.topMargin = (int)event.getY();// -
+					 * (v.getHeight()); marginParams.leftMargin =
+					 * (int)event.getX();// - (v.getWidth()/2);
+					 * 
+					 * RelativeLayout.LayoutParams params = new
+					 * RelativeLayout.LayoutParams(marginParams);
+					 * Log.d("paramstop",
+					 * String.valueOf(marginParams.topMargin));
+					 * v.setLayoutParams(params); root.invalidate();
+					 */
 				}
 				return true;
 			}
 		});
 	}
 
+	protected Builder createItemChooseDialog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) 
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.design, menu);
 		return false;
