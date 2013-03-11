e07d2e184f3fbefdd383f6a1e8c48b8ea0413989
Funklos <fulosio@gmail.com>
2013-03-11 12:55:08 +0100

    strange things happening

--- uiBuilder/src/helpers/IconHelper.java
+++ uiBuilder/src/helpers/IconHelper.java
@@ -78,6 +78,65 @@ public class IconHelper
 	{
 		ArrayList<Integer> iconRefs = new ArrayList<Integer>();
 		
+		iconRefs.add(Integer.valueOf(R.raw.ico_0000));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0001));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0002));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0003));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0004));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0005));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0006));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0007));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0008));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0009));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0010));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0011));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0012));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0013));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0014));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0015));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0016));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0017));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0018));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0019));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0020));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0021));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0022));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0023));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0024));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0025));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0026));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0027));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0028));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0029));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0030));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0031));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0032));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0033));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0034));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0035));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0036));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0037));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0038));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0039));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0040));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0041));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0042));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0043));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0044));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0045));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0046));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0047));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0048));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0049));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0050));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0051));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0052));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0053));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0054));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0055));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0056));
+		iconRefs.add(Integer.valueOf(R.raw.ico_0057));
+		
 		return iconRefs;
 	}
 }
