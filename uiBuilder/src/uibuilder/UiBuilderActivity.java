ad7572d6e1e8b309bd21588c1f3ba525e543aefc
jonesses <mail@jonesses.com>
2013-03-14 19:17:28 +0100

    better presentation export and share

--- uiBuilder/src/uibuilder/UiBuilderActivity.java
+++ uiBuilder/src/uibuilder/UiBuilderActivity.java
@@ -25,7 +25,8 @@ import android.widget.Toast;
 import de.ur.rk.uibuilder.R;
 
 public class UiBuilderActivity extends Activity implements
-		onUiElementSelectedListener, onObjectSelectedListener, onDeleteRequestListener, OnTouchListener
+		onUiElementSelectedListener, onObjectSelectedListener,
+		onDeleteRequestListener, OnTouchListener
 {
 
 	@Override
@@ -38,7 +39,7 @@ public class UiBuilderActivity extends Activity implements
 	public static final int ITEMBOX = 0;
 	public static final int EDITBOX = 1;
 	public static final int DELETEBOX = 2;
-	
+
 	private ItemboxFragment itembox;
 	private EditmodeFragment editbox;
 	private DesignFragment designbox;
@@ -65,7 +66,6 @@ public class UiBuilderActivity extends Activity implements
 		exporter = new ImageTools(getApplicationContext());
 		performInitTransaction();
 
-
 	}
 
 	/**
@@ -73,7 +73,7 @@ public class UiBuilderActivity extends Activity implements
 	 */
 	private void setupUi()
 	{
-		
+
 		itembox = new ItemboxFragment();
 		editbox = new EditmodeFragment();
 		designbox = new DesignFragment();
@@ -84,8 +84,6 @@ public class UiBuilderActivity extends Activity implements
 		DeleteFragment.onDeleteRequestListener(this);
 		setActionBarStyle();
 
-
-		
 	}
 
 	/**
@@ -95,55 +93,57 @@ public class UiBuilderActivity extends Activity implements
 	{
 		ActionBar bar = getActionBar();
 		bar.setCustomView(R.layout.menu_item_preview);
-		
+
 		previewButton = (Button) bar.getCustomView().findViewById(R.id.action_preview_mode);
-		
+
 		previewButton.setOnTouchListener(this);
-		
-		bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_CUSTOM);
+
+		bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME
+				| ActionBar.DISPLAY_SHOW_CUSTOM);
 		bar.setBackgroundDrawable(getResources().getDrawable(R.color.designfragment_background));
 	}
 
 	/**
-	 * Initial fragmenttransaction to display the fragments.
-	 * Editbox is added but hidden to guarantee access
+	 * Initial fragmenttransaction to display the fragments. Editbox is added
+	 * but hidden to guarantee access
 	 */
 	private void performInitTransaction()
 	{
 		FragmentTransaction init = fManager.beginTransaction();
-		
+
 		init.add(R.id.fragment_sidebar, editbox);
 		init.add(R.id.fragment_sidebar, itembox);
 		init.add(R.id.fragment_sidebar, deletebox);
 		init.add(R.id.fragment_design, designbox);
-		
 
 		init.hide(editbox);
 		init.hide(itembox);
 		init.hide(deletebox);
-		
+
 		init.setCustomAnimations(R.animator.to_left_in, R.animator.to_left_out);
 		init.commit();
-		
+
 		objectSelected(false);
 	}
 
 	/**
-	 * Adapt the sidebar to create or edit mode.
-	 * Is called from the interface implementation.
-	 * @param sidebarType specifies which of the sidebars to display
+	 * Adapt the sidebar to create or edit mode. Is called from the interface
+	 * implementation.
+	 * 
+	 * @param sidebarType
+	 *            specifies which of the sidebars to display
 	 */
-	
-	public void displaySidebar(int sidebarType)
+
+	public boolean displaySidebar(int sidebarType)
 	{
 		Log.d("DisplaySidebar", "is Called");
 		FragmentTransaction outSwapper = fManager.beginTransaction();
 		outSwapper.setCustomAnimations(R.animator.to_left_in, R.animator.to_left_out);
-		
+
 		switch (sidebarType)
 		{
 		case ITEMBOX:
-			
+
 			outSwapper.hide(editbox);
 			outSwapper.show(itembox);
 			break;
@@ -151,69 +151,72 @@ public class UiBuilderActivity extends Activity implements
 		case EDITBOX:
 
 			Log.d("switched sideBarType", "result Editbox, replacing");
-			
+
 			outSwapper.show(editbox);
 			outSwapper.hide(itembox);
 
 			break;
-			
+
 		case DELETEBOX:
-			
+
 			outSwapper.hide(editbox);
 			outSwapper.show(deletebox);
 			break;
 		}
 		outSwapper.commit();
-		
+		return true;
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 
-		
-		
 		MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.layout.design, menu);
-	    
-	    return true;
+		inflater.inflate(R.layout.design, menu);
+
+		return true;
 	}
-	
-	
+
 	/**
-	 * switch on the selected item
-	 * action export: call imagetools to process the exporting request
+	 * switch on the selected item action export: call imagetools to process the
+	 * exporting request
+	 * 
 	 * @author funklos
 	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
+		designbox.deleteOverlay();
+		DisplayModeChanger.setPresentationMode(designbox.getView());
+
 		switch (item.getItemId())
 		{
+
 		case R.id.action_export_jpeg:
-			DisplayModeChanger.setPresentationMode(designbox.getView());
+
 
 			exporter.requestBitmap(designbox.getView(), getContentResolver(), false);
-		
+
 			Toast.makeText(getApplicationContext(), getString(R.string.confirmation_save_to_gallery), Toast.LENGTH_SHORT).show();
 			DisplayModeChanger.setCreationMode(designbox.getView());
 
 			break;
 
 		case R.id.action_attach_mail:
-			DisplayModeChanger.setPresentationMode(designbox.getView());
+
 
 			Intent mail = exporter.getIntent(ImageTools.SHARE);
 			mail.putExtra(Intent.EXTRA_STREAM, exporter.requestBitmap(designbox.getView(), getContentResolver(), false));
 
 			startActivityForResult(Intent.createChooser(mail, getString(R.string.intent_title_share)), ImageTools.SHARE);
-			DisplayModeChanger.setCreationMode(designbox.getView());
 
 			break;
-			
+
 		default:
 			break;
 		}
+		DisplayModeChanger.setCreationMode(designbox.getView());
+		displaySidebar(ITEMBOX);
 		return true;
 	}
 
@@ -222,33 +225,36 @@ public class UiBuilderActivity extends Activity implements
 	{
 		// TODO Auto-generated method stub
 		super.onActivityResult(requestCode, resultCode, data);
-		
+
 		if (resultCode == Activity.RESULT_OK)
-		switch (requestCode)
-		{
+			switch (requestCode)
+			{
 
 			case ImageTools.SHARE:
 				Toast.makeText(getApplicationContext(), getString(R.string.confirmation_share_via_mail), Toast.LENGTH_SHORT).show();
 				break;
-				
-		}
+
+			}
 	}
 
-	/**Interface onUiElementSelected method
-	 * @author funklos
-	 * implemented to notify the designbox of the chosen type
-	 * of interface element.
+	/**
+	 * Interface onUiElementSelected method
+	 * 
+	 * @author funklos implemented to notify the designbox of the chosen type of
+	 *         interface element.
 	 */
 	@Override
 	public void typeChanged(int id)
 	{
 		designbox.setSelection(id);
 	}
-	
-	/**Interface onObjectSelected method
-	 * @author funklos
-	 * sets a reference to the object in progress
-	 * @param view the selected view
+
+	/**
+	 * Interface onObjectSelected method
+	 * 
+	 * @author funklos sets a reference to the object in progress
+	 * @param view
+	 *            the selected view
 	 */
 	@Override
 	public void objectChanged(View view)
@@ -258,10 +264,9 @@ public class UiBuilderActivity extends Activity implements
 
 	private View lastTouch;
 
-	
 	/**
-	 * Interface callback method to switch between itembox, when no item is selected,
-	 * and editbox when an item is selected
+	 * Interface callback method to switch between itembox, when no item is
+	 * selected, and editbox when an item is selected
 	 */
 	@Override
 	public void objectSelected(boolean selected)
@@ -271,8 +276,7 @@ public class UiBuilderActivity extends Activity implements
 		{
 			displaySidebar(ITEMBOX);
 			Log.d("item was deleted", "showing itembox");
-		} 
-		else
+		} else
 		{
 			displaySidebar(EDITBOX);
 			editbox.adaptLayoutToContext(lastTouch);
@@ -284,9 +288,10 @@ public class UiBuilderActivity extends Activity implements
 	{
 		displaySidebar(DELETEBOX);
 	}
-	
+
 	/**
 	 * deletebox interface callback
+	 * 
 	 * @author funklos
 	 */
 	@Override
@@ -302,7 +307,7 @@ public class UiBuilderActivity extends Activity implements
 		switch (event.getAction())
 		{
 		case MotionEvent.ACTION_DOWN:
-			designbox.deleteOverlay();	
+			designbox.deleteOverlay();
 			displaySidebar(ITEMBOX);
 			DisplayModeChanger.setPresentationMode(designbox.getView());
 			break;
@@ -311,6 +316,5 @@ public class UiBuilderActivity extends Activity implements
 		}
 		return false;
 	}
-
 
 }
