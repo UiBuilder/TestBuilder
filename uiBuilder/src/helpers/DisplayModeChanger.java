95ff975ffef19a06702cf975d6cdeb2fe8d5604c
jonesses <mail@jonesses.com>
2013-03-14 15:53:11 +0100

    previewMode working onTouch, drawables are still shitty

--- uiBuilder/src/helpers/DisplayModeChanger.java
+++ uiBuilder/src/helpers/DisplayModeChanger.java
@@ -98,4 +98,59 @@ public class DisplayModeChanger
 		}
 	}
 
+	public static void setCreationMode(View layout)
+	{
+		if (layout instanceof ViewGroup && layout.getTag() == null) {
+			int count = ((ViewGroup) layout).getChildCount();
+
+			for (int i = 0; i < count; i++) {
+				setCreationMode(((ViewGroup) layout).getChildAt(i));
+
+			}
+		} else if (layout instanceof TextView || layout instanceof LinearLayout || layout instanceof RelativeLayout) {
+			Bundle tagBundle = (Bundle) layout.getTag();
+
+			int id = tagBundle.getInt(Generator.ID);
+			switch (id)
+
+			{
+			case R.id.element_button:
+				
+
+			case R.id.element_checkbox:
+
+
+			case R.id.element_edittext:
+
+
+			case R.id.element_imageview:
+
+
+			case R.id.element_numberpick:
+			
+			case R.id.element_radiogroup:
+
+			case R.id.element_ratingbar:
+			
+			case R.id.element_switch:
+
+			case R.id.element_textview:
+
+			case R.id.element_timepicker:
+
+
+			case R.id.element_seekbar:
+
+
+			case R.id.element_list:
+				layout.setBackgroundResource(R.drawable.default_button_border);
+
+			default:
+
+				break;
+			}
+		}
+		
+	}
+
 }
