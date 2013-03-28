4c8eea7d420759a7163f99f37dd07e7b3dacddea
jonesses <mail@jonesses.com>
2013-03-28 15:18:28 +0100

    editmode

--- uiBuilder/src/editmodules/StarCountModule.java
+++ uiBuilder/src/editmodules/StarCountModule.java
@@ -1,45 +1,102 @@
 package editmodules;
 
+import de.ur.rk.uibuilder.R;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class StarCountModule extends Module
 {
-	
-	
+	SeekBar starBar, ratingSlider;
+
+	private View box;
+
+	private View requesting;
 
 	public StarCountModule(Context context)
 	{
 		super(context);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void getValues()
 	{
-		// TODO Auto-generated method stub
+		adaptToContext();
 
 	}
 
 	@Override
 	protected void setupUi()
 	{
-		// TODO Auto-generated method stub
+		box = super.inflater.inflate(R.layout.editmode_entry_stars_count, null);
+		box.setOnClickListener(new ExpansionListener(box));
 
+		starBar = (SeekBar) box.findViewById(R.id.star_count_seekbar);
+		ratingSlider = (SeekBar) box.findViewById(R.id.star_rating_seekbar);
+
+		RatingModuleListener ratingListener = new RatingModuleListener();
+
+		starBar.setOnSeekBarChangeListener(ratingListener);
 	}
 
 	@Override
 	public View getInstance(View inProgress)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		requesting = inProgress;
+		adaptToContext();
+
+		return box;
+	}
+
+	private class RatingModuleListener implements OnSeekBarChangeListener
+	{
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress,
+				boolean fromUser)
+		{
+			switch (seekBar.getId())
+			{
+			case R.id.star_count_seekbar:
+				((RatingBar) ((ViewGroup) requesting).getChildAt(0)).setNumStars(progress + 1);
+				ratingSlider.setMax(progress + 1);
+
+				Log.d("StarcountSeekbar", "gotValue for progress: " + progress
+						+ 1);
+				break;
+
+			case R.id.star_rating_seekbar:
+				Log.d("RatingSeekbar", "gotValue for progress: " + progress);
+				((RatingBar) ((ViewGroup) requesting).getChildAt(0)).setRating(progress);
+				break;
+			}
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar)
+		{
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar)
+		{
+			// TODO Auto-generated method stub
+
+		}
 	}
 
 	@Override
 	protected void adaptToContext()
 	{
-		// TODO Auto-generated method stub
-		
+		ratingSlider.setProgress((int) (((RatingBar) ((ViewGroup) requesting).getChildAt(0)).getRating()));
+
+		starBar.setProgress(((RatingBar) ((ViewGroup) requesting).getChildAt(0)).getNumStars() - 1);
+
 	}
 
 }
